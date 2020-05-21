88dfa6cfad8dc43b8d8e6b111ce9d751bf70d8ec
John Somers <j.a.somers@hva.nl>
2020-05-21 22:12:19 +0200

    ConcurrentMap updates

--- Week4/ConcurrentMap/src/main/java/CHM_Main.java
+++ Week4/ConcurrentMap/src/main/java/CHM_Main.java
@@ -4,36 +4,40 @@ import java.util.stream.Collectors;
 
 /**
  * https://docs.oracle.com/javase/tutorial/collections/streams/parallelism.html
- *
+ * <p>
  * demonstrates use of concurrentMap and parallelStream
  *
  * @author j.a.somers@hva.nl
- *
  */
 public class CHM_Main {
 
+    static ForkJoinPool streamEngine = ForkJoinPool.commonPool();
+    //static ForkJoinPool streamEngine = new ForkJoinPool(8);
+
     // dummy synchronised list, used to drive the parallelStream for setup
-    private static final int NTASKS = 40;
+    private static final int NTASKS = 64;
     private static List<Integer> dummyList =
             Collections.synchronizedList(new ArrayList<>());
 
-    private static final int NWORDS = 10000000;
+    private static final int NWORDS = 25600000;
     // word dictionary counts words
-    private static ConcurrentHashMap<String,Integer> wordMap =
-            new ConcurrentHashMap<String,Integer>(64, (float)0.9, NTASKS);
+    private static ConcurrentMap<String, Integer> wordMap =
+            new ConcurrentHashMap<>(64, (float) 0.9, NTASKS);
+    private static Map<String, Integer> synchronizedWordMap =
+            Collections.synchronizedMap(new HashMap<>());
     // dictionary index counts total number of words by first character
-    private static ConcurrentHashMap<Character,Integer> index =
-            new ConcurrentHashMap<Character,Integer>(64, (float)0.9, NTASKS);
+    private static ConcurrentHashMap<Character, Integer> index =
+            new ConcurrentHashMap<>(64, (float) 0.9, NTASKS);
 
-    private static void setupWordMap(Map<String,Integer> map, int nWords) {
+    private static void setupWordMap(Map<String, Integer> map, int nWords) {
         Random random = new Random();
 
-        for (int i=0; i<nWords; i++) {
+        for (int i = 0; i < nWords; i++) {
             // create a random word with 2-5 characters
             int l = 2 + random.nextInt(4);
             String w = "";
-            for (int j=0; j<l; j++) {
-                w += (char)('A' + random.nextInt(26));
+            for (int j = 0; j < l; j++) {
+                w += (char) ('A' + random.nextInt(26));
             }
 
             // enter a new word or add 1 to its current count
@@ -43,26 +47,26 @@ public class CHM_Main {
 
     private static void printMap(Map map) {
         map.forEach(
-                (k,v) -> {
+                (k, v) -> {
                     System.out.println(k + ": " + v);
                 }
         );
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
 
         // create the dummy list for arbitrary parallelStream
-        for (int i=0; i<NTASKS; i++) {
+        for (int i = 0; i < NTASKS; i++) {
             dummyList.add(i);
         }
 
         System.out.println("Streaming order:");
-        dummyList.stream().forEach((i)->{
-            System.out.print(" "+i);
+        dummyList.stream().forEach((i) -> {
+            System.out.print(" " + i);
         });
         System.out.println();
-        dummyList.parallelStream().forEach((i)->{
-            System.out.print(" "+i);
+        dummyList.parallelStream().forEach((i) -> {
+            System.out.print(" " + i);
         });
         System.out.println();
 
@@ -70,72 +74,96 @@ public class CHM_Main {
         System.out.println("NWORDS = " + NWORDS);
         wordMap.clear();
         start = System.nanoTime();
-        dummyList.stream().forEach((i)->{
-            setupWordMap(wordMap, NWORDS/NTASKS);
+        dummyList.stream().forEach((i) -> {
+            setupWordMap(wordMap, NWORDS / NTASKS);
         });
-        System.out.println("Serial setup: "+((System.nanoTime() - start) / 1E9) + " sec.");
+        System.out.println("Serial setup: " + ((System.nanoTime() - start) / 1E9) + " sec.");
 
         wordMap.clear();
         start = System.nanoTime();
-        dummyList.parallelStream().forEach((i)->{
-            setupWordMap(wordMap, NWORDS/NTASKS);
+        dummyList.parallelStream().forEach((i) -> {
+            setupWordMap(wordMap, NWORDS / NTASKS);
         });
-        System.out.println("Parallel setup: "+((System.nanoTime() - start) / 1E9) + " sec.");
+        System.out.println("Parallel setup: " + ((System.nanoTime() - start) / 1E9) + " sec.");
+
+        wordMap.clear();
+        start = System.nanoTime();
+        dummyList.parallelStream().forEach((i) -> {
+            Map<String, Integer> localMap = new HashMap<>();
+            setupWordMap(localMap, NWORDS / NTASKS);
+            localMap.forEach((k, v) -> wordMap.merge(k, v, Integer::sum));
+        });
+        System.out.println("Batch Parallel setup: " + ((System.nanoTime() - start) / 1E9) + " sec.");
+
+        synchronizedWordMap.clear();
+        start = System.nanoTime();
+        dummyList.parallelStream().forEach((i) -> {
+            Map<String, Integer> localMap = new HashMap<>();
+            setupWordMap(localMap, NWORDS / NTASKS);
+            localMap.forEach((k, v) -> synchronizedWordMap.merge(k, v, Integer::sum));
+        });
+        System.out.println("Batch Synchronized setup: " + ((System.nanoTime() - start) / 1E9) + " sec.");
 
         index.clear();
         start = System.nanoTime();
         wordMap
-            .entrySet() // unfortunately maps cannot be streamed yet?
-            .stream()
-            .forEach(
-                (Map.Entry e) -> {
-                    // add the word-count to the character index
-                    index.merge(
-                        ((String)e.getKey()).charAt(0),
-                        (Integer)e.getValue(),
-                        Integer::sum);
-                    }
-                );
-        System.out.println("Serial index: "+((System.nanoTime() - start) / 1E9) + " sec.");
-
-        System.out.println("NumWords in serial index = " +
-             index.reduceValues(1, Integer::sum));
-
-        index.clear();
-        start = System.nanoTime();
-        wordMap
-                .entrySet() // unfortunately maps cannot be streamed yet?
-                .parallelStream()
                 .forEach(
-                        (Map.Entry e) -> {
+                        (k, v) -> {
                             // add the word-count to the character index
                             index.merge(
-                                    ((String)e.getKey()).charAt(0),
-                                    (Integer)e.getValue(),
+                                    k.charAt(0),
+                                    v,
                                     Integer::sum);
                         }
                 );
-        System.out.println("Parallel index: "+((System.nanoTime() - start) / 1E9) + " sec.");
+        System.out.println("Serial index: " + ((System.nanoTime() - start) / 1E9) + " sec.");
+
+        System.out.println("NumWords in serial index = " +
+                index.reduceValues(1, Integer::sum));
+
+        index.clear();
+        start = System.nanoTime();
+        streamEngine.submit(() -> {
+            wordMap
+                    .entrySet()
+                    .parallelStream()
+                    .forEach(
+                            (e) -> {
+                                // add the word-count to the character index
+                                index.merge(e.getKey().charAt(0), e.getValue(), Integer::sum);
+                            });
+        }).join();
+        System.out.println("Parallel index: " + ((System.nanoTime() - start) / 1E9) + " sec.");
 
         System.out.println("NumWords in parallel index = " +
                 index.reduceValues(NTASKS, Integer::sum));
 
         start = System.nanoTime();
-        ConcurrentMap<Character,Integer> index2 =
-                wordMap
-                .entrySet()
-                .parallelStream()
-                .collect(Collectors.groupingByConcurrent(e->e.getKey().charAt(0),
-                        Collectors.summingInt(e->e.getValue()))
-                    );
-        System.out.println("Parallel collected index: "+((System.nanoTime() - start) / 1E9) + " sec.");
+        ConcurrentMap<Character,Integer> index2;
+        index2 =
+                streamEngine.submit(() -> {
+                    return
+                        wordMap
+                            .entrySet()
+                            .parallelStream()
+                            .collect(
+                                Collectors.toConcurrentMap(
+                                    e -> e.getKey().charAt(0),
+                                    Map.Entry::getValue,
+                                    Integer::sum));
+
+                }).get();
+        System.out.println("Parallel collected index: " + ((System.nanoTime() - start) / 1E9) + " sec.");
 
 
         System.out.println("NumWords in parallel collected index = " +
-                index2.entrySet()
-                        .stream()
-                        .mapToLong(e->e.getValue())
-                        .sum()
-                        );
+                streamEngine.submit(() -> {
+                    return
+                        index2.entrySet()
+                                .parallelStream()
+                                .mapToLong(Map.Entry::getValue)
+                                .sum();
+                }).get()
+        );
     }
 }
